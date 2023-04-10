6e9abb38d17d3b6fffd7aa6ef1324d3ecc6a2649
nardien <nardeenashraf2014@gmail.com>
2023-04-10 23:56:39 +0200

    SubDepartments Collection Done

--- controllers/subDepartments.js
+++ controllers/subDepartments.js
@@ -1,23 +1,62 @@
 const SubDepartmentModel = require("../models/subDepartments");
 
-function getAllSubDeps() {
-    return SubDepartmentModel.find();
+//Get All SubDepartments
+const getAllSubDeps= async (req, res, next) => { 
+  try{
+      const subDepartments = await SubDepartmentModel.find();;
+      res.status(200).json(subDepartments);
   }
-  function AddnewSubDep(department) {
-    return SubDepartmentModel.create(department);
-    
+  catch (err){
+      res.json({message: err.message})
   }
-  
-  function getSubDepById(id) {
-    return SubDepartmentModel.findById(id);
+}
+//Get subDepartment by id
+const getSubDepById= async (req,res,next)=>{
+    const {id} = req.params;
+    try{
+        const specificSubDep = await SubDepartmentModel.findById(id);;
+        res.status(200).json(specificSubDep)
+    }
+    catch(err){
+        res.json({message: err.message})
+    }
+}
+//Add new subDepartment
+const AddnewSubDep= async (req, res, next) => {
+  const newSubDepartment = req.body;
+  try{
+      const addededSubDep = await SubDepartmentModel.create(newSubDepartment);;
+      res.status(201).json(addededSubDep)
   }
-  
-  function updateSubDepById(id, obj) {
-    return SubDepartmentModel.findByIdAndUpdate(id, obj, { new: true });//new(options) if true, return the modified document rather than the original
+  catch (err){
+      res.status(422).json({message: err.message})
+  }  
+}
+
+//Update subDepartment
+const updateSubDepById= async (req, res,) => {
+  const id = req.params.id
+  const obj = req.body
+  try{
+      let updatedSubDep = await SubDepartmentModel.findByIdAndUpdate(id, obj, { new: true });
+      res.json(updatedSubDep)
   }
-  
-  function deleteSubDepartment(id) {
-    return SubDepartmentModel.findByIdAndDelete(id);
+  catch (err){
+      res.status(422).json({message: err.message})
   }
 
-  module.exports={AddnewSubDep,getAllSubDeps,getSubDepById,updateSubDepById,deleteSubDepartment}
+}
+
+//Delete SubDepartment
+const deleteSubDepartment= async (req,res)=>{
+  const id = req.params.id
+  try{
+      let deletedSubDep = await SubDepartmentModel.findByIdAndDelete(id);;
+      res.json("Sub-Department Deleted Successfully")
+  }
+  catch (err){
+      res.status(422).json({message: err.message})
+  }   
+}
+
+module.exports={AddnewSubDep,getAllSubDeps,getSubDepById,updateSubDepById,deleteSubDepartment}
--- routes/subDepartments.js
+++ routes/subDepartments.js
@@ -2,66 +2,20 @@ const express = require('express')
 const { getAllSubDeps,AddnewSubDep,getSubDepById,updateSubDepById,deleteSubDepartment } = require('../controllers/subDepartments');
 const router = express.Router()
 
-// get all sub-departments
-router.get("/", async (req, res, next) => { 
-    try{
-        const subDepartments = await getAllSubDeps();
-        res.status(200).json(subDepartments);
-    }
-    catch (err){
-        res.json({message: err.message})
-    }
-})
+//Get All SubDepartments
+router.get("/", getAllSubDeps);
 
-// get sub-department by id
-router.get("/:id",async (req,res,next)=>{
-    const {id} = req.params;
-    try{
-        const specificSubDep = await getSubDepById(id);
-        res.status(200).json(specificSubDep)
-    }
-    catch(err){
-        res.json({message: err.message})
-    }
-})
+//Get subDepartment by id
+router.get("/:id",getSubDepById)
 
-// add new department
-router.post("/", async (req, res, next) => {
-    const newSubDepartment = req.body;
-    try{
-        const addededSubDep = await AddnewSubDep(newSubDepartment);
-        res.status(201).json(addededSubDep)
-    }
-    catch (err){
-        res.status(422).json({message: err.message})
-    }  
-})
+//Add new SubDepartment
+router.post("/", AddnewSubDep);
 
-// update sub-department
-router.patch("/:id",async (req, res,) => {
-    const id = req.params.id
-    const obj = req.body
-    try{
-        let updatedSubDep = await updateSubDepById(id,obj)
-        res.json(updatedSubDep)
-    }
-    catch (err){
-        res.status(422).json({message: err.message})
-    }
+//Update subDepartment
+router.patch("/:id",updateSubDepById)
 
-})
-
-// delete sub-department
-router.delete("/:id", async (req,res)=>{
-    const id = req.params.id
-    try{
-        let deletedSubDep = await deleteSubDepartment(id);
-        res.json("Sub-Department Deleted Successfully")
-    }
-    catch (err){
-        res.status(422).json({message: err.message})
-    }   
-});
+//Delete subDepartment
+router.delete("/:id", deleteSubDepartment);
 
 module.exports = router
 
