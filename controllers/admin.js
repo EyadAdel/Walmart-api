fcadb6cc938d8dc34ffe9ecd4be6260ddb192d9b
Abdallamhmoud <73116176+Abdallamhmoud@users.noreply.github.com>
2023-04-11 01:16:45 +0200

    abdalla updates

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,22 +1,59 @@
 const adminModel=require("../models/admin")
 
-  function getAllAdmins() {
-    return adminModel.find();
-  }
-  function AddnewAdmin(admin) {
-    return adminModel.create(admin);
-  }
+  const getAllAdmins = async (req, res, next) => { 
+    try{
+        var newAdmin = await adminModel.find();
+        res.status(200).json(newAdmin) 
+    }
+    catch (err){
+        res.json({message: err.message})
+    }
+}
+  const AddnewAdmin = async (req, res, next) => {
+    var admin = req.body
+    try{
+        var savedAdmin = await adminModel.create(admin);
+        res.status(201).json(savedAdmin)
+    }
+    catch (err){
+        res.status(422).json({message: err.message})
+    }
+    
+}
   
-  function getAdminById(id) {
-    return adminModel.findById(id);
-  }
+  const getAdminById = async (req,res,next)=>{
+    var {id} = req.params
+    try{
+        var specificAdmin = await adminModel.findById(id);
+        res.status(200).json(specificAdmin)
+    }
+    catch(err){
+        res.json({message: err.message})
+    }
+}
   
-  function updateAdminById(id, obj) {
-    return adminModel.findByIdAndUpdate(id, obj, { new: true });//new(options) if true, return the modified document rather than the original
-  }
+  const updateAdminById = async (req, res,) => {
+    var id = req.params.id
+    var obj = req.body
+    try{
+        let updatedAdmin = await adminModel.findByIdAndUpdate(id, obj, { new: true });//new(options) if true, return the modified document rather than the original
+        res.json(updatedAdmin)
+    }
+    catch (err){
+        res.status(422).json({message: err.message})
+    }
+
+}
   
-  function deleteAdminById(id) {
-    return adminModel.findByIdAndDelete(id);
-  }
+  const deleteAdminById = async (req,res)=>{
+    var id = req.params.id
+    try{
+        let deletedAdmin = await adminModel.findByIdAndDelete(id);;
+        res.json("Admin deleted successfully");
+    }
+    catch (err){
+        res.status(422).json({message: err.message})
+    }   
+}
 
   module.exports={AddnewAdmin,getAllAdmins,getAdminById,updateAdminById,deleteAdminById}
